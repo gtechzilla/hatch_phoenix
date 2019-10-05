c1f4984d707c7fc862f3590c9f7dc1a5236f34bd
gtechzilla <okiomagerald@gmail.com>
2019-10-05 14:02:49 +0300

    our landng page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Navbar,Nav,Carousel,Jumbotron,Container} from 'react-bootstrap';
 import './App.css';
+import slide1 from './images/slide1.jpg'
+import slide2 from './images/slide2.jpg'
+import slide3 from './images/slide3.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div>
+<Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Mavuno</Nav.Link>
+
+    </Nav>
+
+  </Navbar>
+  <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={slide1}
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={slide2}
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={slide3}
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+<Jumbotron fluid>
+  <Container>
+    <h1>Fluid jumbotron</h1>
+    <p>
+      This is a modified jumbotron that occupies the entire horizontal space of
+      its parent.
+    </p>
+  </Container>
+</Jumbotron>
+</div>
   );
 }
 
